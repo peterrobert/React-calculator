d85570e6eba66bc86c6f0f0b5c39a28b7caea540
peterrobert <pwambui93@gmail.com>
2021-01-13 08:36:22 +0300

    fixed display proptypes calculations

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,11 @@ import '../App.css';
 import { Display } from './Display';
 import { ButtonPanel } from './ButtonPanel';
 
-const App = () => {
-  const calculate = '0';
-  return (
-    <>
-      <Display calculations={calculate} />
-      <ButtonPanel />
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Display />
+    <ButtonPanel />
+  </>
+);
 
 export default App;
--- src/components/Display.js
+++ src/components/Display.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 
 export const Display = props => {
   const { calculations } = props;
-  return <h1>{calculations}</h1>;
+
+  return (<h1>{calculations}</h1>);
+};
+
+Display.defaultProps = {
+  calculations: '0',
 };
 
 Display.propTypes = {
-  calculations: PropTypes.string.isRequired,
+  calculations: PropTypes.string,
 };
