339d89745746614dc4b86479a4ae1fdb61aadeea
peterrobert <pwambui93@gmail.com>
2021-01-13 08:19:15 +0300

    fixed calculator error

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,11 @@
 import { React } from 'react';
+import PropTypes from 'prop-types';
 
 export const Display = props => {
   const { calculations } = props;
   return <h1>{calculations}</h1>;
+};
+
+Display.propTypes = {
+  calculations: PropTypes.string.isRequired,
 };
