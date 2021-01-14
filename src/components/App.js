8c1ed2fae4582cce2a0ed93db73cae99effe22aa
peterrobert <pwambui93@gmail.com>
2021-01-14 22:18:11 +0300

    created the class component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,31 @@ import React from 'react';
 import '../App.css';
 import { Display } from './Display';
 import { ButtonPanel } from './ButtonPanel';
+import { calculate } from '../logic/calculate';
 
-const App = () => (
-  <div className="app_component">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component{
+  constructor(){
+    super();
+
+    let state = {
+      total: null,
+      next: null,
+      operation: null
+    };
+  }
+
+  
+
+
+  render(){
+    return(
+      <div className="app_component">
+      <Display />
+      <ButtonPanel />
+    </div>
+    )
+  }
+
+}
 
 export default App;
