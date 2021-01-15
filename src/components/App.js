6b8a64023f953febce9de47f3fc819476083b62b
peterrobert <pwambui93@gmail.com>
2021-01-15 08:21:10 +0300

    added the import statement

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import { Display } from './Display';
 import { ButtonPanel } from './ButtonPanel';
+import { calculate } from "../logic/calculate";
 
 const App = () => (
   <div className="app_component">
