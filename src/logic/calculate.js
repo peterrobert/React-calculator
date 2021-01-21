39c0bb3c0d3cca94af6494a50328e22c82819686
peterrobert <pwambui93@gmail.com>
2021-01-21 18:03:37 +0300

    improved on calculate logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,41 +1,99 @@
-/* eslint-disable no-unused-vars */
 import { operate } from './operate';
 
-export const calculate = (calculator, button) => {
-  let { total, next, operation } = calculator;
+export const calculate = (dataObj, btnName) => {
+  const { total, next, operation } = dataObj;
 
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const operationSyms = ['+', '-', 'X', '÷', '%'];
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-  if (button === '+/-') {
-    total *= -1;
-    next = 0;
-    operation = null;
-    return calculator;
+  if (operationSyms.includes(btnName) && next && total) {
+    return {
+      total: operate(total, next, operation).toString(),
+      operation: btnName,
+      next: null,
+    };
   }
 
-  if (button === 'A/C') {
-    total = 0;
-    next = 0;
-    operation = null;
-    return calculator;
+  if (operationSyms.includes(btnName) && !next && total) {
+    return {
+      total,
+      operation: btnName,
+      next,
+    };
   }
 
-  if (button === numbers.includes(button)) {
-    total = button;
-    next = 0;
-    operation = null;
-
-    return calculator;
+  if (operationSyms.includes(btnName) && next && !total) {
+    return {
+      total: next,
+      operation: btnName,
+      next: null,
+    };
   }
 
-  if (next === numbers.includes(next) && total === button) {
-    total = 'Error';
-    next = 0;
-    operation = null;
-
-    return calculator;
+  if (total === 'ERROR') {
+    return {
+      total: null,
+      operation: null,
+      next: null,
+    };
   }
 
-  total = operate(total, next, button);
-  return total;
+  if (numbers.includes(btnName)) {
+    return {
+      total,
+      next: next ? `${next}${btnName}` : `${btnName}`,
+      operation,
+    };
+  }
+
+  if (btnName === '+/-' && !next && total && total !== 'ERROR') {
+    return {
+      total: (total * -1).toString(),
+      next,
+      operation,
+    };
+  }
+
+  if (btnName === '+/-' && next && total !== 'ERROR') {
+    return {
+      total,
+      next: (next * -1).toString(),
+      operation,
+    };
+  }
+
+  if (btnName === 'AC') {
+    return {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  if (btnName === '=') {
+    return {
+      total: operate(total, next, operation),
+      next: null,
+      operation: null,
+    };
+  }
+
+  if (btnName === '.') {
+    if (!next) {
+      return {
+        total,
+        next: '0.',
+        operation,
+      };
+    }
+    if (next && !next.includes('.')) {
+      return {
+        total,
+        next: `${next}.`,
+        operation,
+      };
+    }
+  }
+
+  return [total, next, operation];
 };
