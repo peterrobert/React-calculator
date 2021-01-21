ee5357cc14ece5d8a96dbc1a170ebe1536031a9a
peterrobert <pwambui93@gmail.com>
2021-01-21 18:02:47 +0300

    improved on operate logic

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,36 +1,34 @@
 import Big from 'big.js';
 
-export const operate = (numberOne, numeberTwo, operation) => {
-  const first = new Big(numberOne);
-  const second = new Big(numeberTwo);
-  let result;
+export const operate = (numberOne, numberTwo, operation) => {
+  let data = 0;
+  const num1 = Big(numberOne || '0');
+  const num2 = Big(numberTwo || '0');
 
-  if (operation === '-') {
-    result = first.minus(second);
-    return result;
+  switch (operation) {
+    case '÷':
+      try {
+        data = num1.div(num2);
+      } catch (e) {
+        data = 'ERROR';
+      }
+      break;
+    case 'X':
+      data = num1.times(num2);
+      break;
+    case '+':
+      data = num1.plus(num2);
+      break;
+    case '-':
+      data = num1.minus(num2);
+      break;
+    case '%':
+      data = num1.times(Big(0.01));
+      break;
+    default:
+      break;
   }
 
-  if (operation === '÷') {
-    result = first.div(second);
-    return result;
-  }
-
-  if (operation === '+') {
-    result = first.plus(second);
-    return result;
-  }
-
-  if (operation === 'X') {
-    result = first.times(second);
-    return result;
-  }
-
-  if (operation === '%') {
-    result = first.plus(second);
-    const perc = result.div(100);
-
-    return perc;
-  }
-
-  return result;
+  return data.toString();
 };
+
